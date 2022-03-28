7af1d8c3f8a6c60bd1e5ac70d8430d38843696b3
DrLmD10 <102502041+DrLmD10@users.noreply.github.com>
2022-03-28 16:10:28 +0800

    add a confif file

---
+++ config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 16:06:26 2022
+
+@author: admin
+"""
+
+class vigil_config:
+    def __init__(self):
+        self.lr = 1e-3
+        self.alpha = 0.9
+        self.EPOCH = 100
